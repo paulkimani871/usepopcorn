88d396551031f6323c031e705df0ab01b6a5c288
paulkimani871 <kimaninjoroge871@gmial.com>
2026-09-16 16:34:17 +0300

    Refactor useMovies hook: update API key, fix error messages, and improve code formatting

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,44 +1,55 @@
-import { useEffect, useState } from "react";
-const KEY = "f84fc31d";
+import {useEffect, useState} from 'react'
 
-export function useMovies (query){
-    const [movies,setMovies] = useState([])
-    const [isLoading,setIsLoading] = useState(false)
-    const [error,setError] = useState ("")
+export  function useMovies (query){
+    const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const KEY = "4e8fee6";
 
     useEffect(
         function () {
+
+            // callback?.()
           const controller = new AbortController();
           async function fetchMovies() {
             try {
               setIsLoading(true);
               setError("");
               const res = await fetch(
-                `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-                { signal: controller.signal },
+                `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                { signal: controller.signal }
               );
               if (!res.ok)
                 throw new Error("Something went wrong with fetching movies");
+    
               const data = await res.json();
-              if (data.Response === "False") throw new Error("movie not found");
+    
+              if (data.Response === "False") throw new Error("Movie not found");
+    
               setMovies(data.Search);
+              setError("")
             } catch (err) {
+              // console.log(err.message);
               if (err.name !== "AbortError") {
-                console.error(err.message);
                 setError(err.message);
               }
             } finally {
               setIsLoading(false);
             }
           }
-        //   handleCloseMovie()
-    
+          if (query.length < 3) {
+            setMovies([]);
+            setError("");
+            return;
+          }
+        
           fetchMovies();
+    
           return function () {
             controller.abort();
           };
         },
-        [query],
+        [query]
       );
 
       return {movies, isLoading, error}
